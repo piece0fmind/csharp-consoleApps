b49bf21d8e0a2b155ec952a4c032c61ce208bc8e
piece0fmind <bibek9lives@icloud.com>
2024-11-09 13:00:50 +0545

    Check coun

--- ConsoleApps/Classes/StackWithList.cs
+++ ConsoleApps/Classes/StackWithList.cs
@@ -15,6 +15,16 @@ namespace ConsoleApps.Classes
         [Benchmark]
         public void Display()
         {
+            stack = new List<int>();
+            /*if (stack is null)
+            {
+                Console.WriteLine("Stack is empty.");
+            }*/
+            if (stack.Count == 0)
+            {
+                Console.WriteLine("Null");
+            }
+
             if (stack.Count == 0)
             {
                 Console.WriteLine("The stack is empty.");
@@ -32,7 +42,7 @@ namespace ConsoleApps.Classes
                 Console.WriteLine("The stack is empty.");
                 return -1;
             }
-            int value = stack[stack.Count - 1];
+            int value = stack[index: stack.Count - 1];
             stack.RemoveAt(stack.Count - 1);
             Console.WriteLine(value + " popped from stack.");
             return value;
