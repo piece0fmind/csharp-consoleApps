3f746f7d45ac44c82a95050955964c02f0cd6d55
piece0fmind <bibek9lives@icloud.com>
2024-11-09 23:21:30 +0545

    Add Utility class for stack

---
+++ ConsoleApps/Utility/StackUtils.cs
@@ -0,0 +1,23 @@
+﻿using ConsoleApps.Interfaces;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ConsoleApps.Utility
+{
+    public static class StackUtils
+    {
+        public static void PerformStackOperations(ICustomStack stack)
+        {
+            Console.WriteLine($"Performing operations on stack of type: {stack.GetType().Name}");
+
+            stack.Push(10);
+            stack.Display();
+
+            stack.Pop();
+            stack.Display();
+        }
+    }
+}
